68ea8bae16724f2586f102a451578157440a8d44
shreya1-svg <karkishreya66@gmail.com>
2025-09-28 07:38:21 +1000

    Update login.php

--- pms5/login.php
+++ pms5/login.php
@@ -38,7 +38,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Pakenham Hospital</title>
+    <title> Pakenham Hospital-Login</title>
    <link rel="stylesheet" href="/pms5/css/modern-style.css?v=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
